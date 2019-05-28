b50f82f3a8bff0288c5f2660af01d3dbc9b13445
SammyIsra <sammy.israwi@gmail.com>
2019-05-28 14:02:58 -0400

    Added two additional reducers to simulate traffic lights. Useless, but fun

--- src/Components/ReducerFun.tsx
+++ src/Components/ReducerFun.tsx
@@ -1,9 +1,8 @@
 import React, { useReducer } from "react";
 import SimpleList from "../Styles/SimpleList";
 
-type reducerStates = "advance" | "go back";
-
-const componentReducer: React.Reducer<number, reducerStates> = (
+type CounterReducerStates = "advance" | "go back";
+const counterReducer: React.Reducer<number, CounterReducerStates> = (
   prevState,
   action
 ) => {
@@ -15,14 +14,57 @@ const componentReducer: React.Reducer<number, reducerStates> = (
   }
 };
 
+type StreetLightStates = "green" | "yellow" | "red";
+const streetLightReducer: React.Reducer<StreetLightStates, void> = (
+  prevState,
+  action
+) => {
+  switch (prevState) {
+    case "green":
+      return "yellow";
+    case "yellow":
+      return "red";
+    case "red":
+      return "green";
+  }
+};
+
+type AdvancedStreetLightStates = "green" | "yellow" | "red";
+const advancedStreetLightReducer: React.Reducer<
+  StreetLightStates,
+  StreetLightStates
+> = (prevState, action) => {
+  switch (prevState) {
+    case "green":
+      return action === "yellow" ? action : prevState;
+    case "yellow":
+      return action === "red" ? action : prevState;
+    case "red":
+      return action === "green" ? action : prevState;
+  }
+};
+
 const ReducerFun = () => {
-  const [state, dispatch] = useReducer(componentReducer, 0);
+  const [clickedCounter, dispatchCounter] = useReducer(counterReducer, 0);
+  const [light, dispatchLight] = useReducer(streetLightReducer, "red");
+  const [aLight, aDispatchLight] = useReducer(
+    advancedStreetLightReducer,
+    "red"
+  );
 
   return (
     <SimpleList>
-      <div>Clicked {state} times!</div>
-      <button onClick={() => dispatch("advance")}>Increase</button>
-      <button onClick={() => dispatch("go back")}>Go back</button>
+      <div>Clicked {clickedCounter} times!</div>
+      <button onClick={() => dispatchCounter("advance")}>Increase</button>
+      <button onClick={() => dispatchCounter("go back")}>Go back</button>
+      <br />
+      <div>Current light: {light}</div>
+      <button onClick={() => dispatchLight()}>Move forward</button>
+      <br />
+      <div>Current advanced light: {aLight}</div>
+      <button onClick={() => aDispatchLight("green")}>Go Green</button>
+      <button onClick={() => aDispatchLight("yellow")}>Go Yellow</button>
+      <button onClick={() => aDispatchLight("red")}>Go Red</button>
     </SimpleList>
   );
 };
